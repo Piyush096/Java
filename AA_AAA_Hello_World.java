fa54684c71efa688b6a6afed009b5ac45481e612
Piyush096 <mishra.piyush1886@gmail.com>
2022-03-07 11:29:41 +0530

    Update

--- AA_AAA_Hello_World.java
+++ AA_AAA_Hello_World.java
@@ -2,6 +2,11 @@ public class AA_AAA_Hello_World
 {
     public static void main(String args[])
     {
-       System.out.println("hello");
+        System.out.print("hello");
+        System.out.println("hello");
+        System.out.print("hello");
     }
 }
+/*
+*using println java will put a new line at the end . but in case of print java will not put new line
+*/
