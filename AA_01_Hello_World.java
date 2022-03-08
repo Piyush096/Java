e1564375cee38337553fc5c9a4385bf027780986
Piyush096 <mishra.piyush1886@gmail.com>
2022-03-08 22:22:30 +0530

    Update

--- AA_01_Hello_World.java
+++ AA_01_Hello_World.java
@@ -8,6 +8,10 @@ class A
     }
 }
 /*
+*public static void main(String args[]):- this is the line at which the program will begin executing,
+as a general rule , a java programs begins execution by calling main().
+
+*
 *using println java will put a new line at the end . but in case of print java will not put new line.
 
 */
---
+++ AA_03_IFSAMPLE.java
@@ -0,0 +1,16 @@
+public class AA_03_IFSAMPLE
+{
+    public static void main(String arg[])
+    {
+       int x,y;
+       x = 10;
+       y = 20;
+
+       if(x < y)System.out.println("x is less than y");
+
+       x = x * 2;
+       if(x == y)System.out.println("x is is equal to y");
+
+
+    }
+}
