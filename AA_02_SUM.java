03e498584d145d84ad88a19561b85a25113a603f
Piyush096 <mishra.piyush1886@gmail.com>
2022-03-09 16:12:05 +0530

    Update

--- AA_02_SUM.java
+++ AA_02_SUM.java
@@ -22,6 +22,7 @@ the class(scanner) comes under the pacakage( predefined function in c) :- import
 *after that we have to initilize it in a variable of data type we want to take
 ex :-  Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
+    *The "next" method gets the string of text that a user enters on the keyboard.
 
 in this example first we take the value from the user and stored it in the variable of int type .
 we can also store
