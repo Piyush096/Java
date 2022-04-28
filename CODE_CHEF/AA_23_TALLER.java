5db961a4f333bf5bd33f751ad977fb5a57178cf5
Piyush096 <mishra.piyush1886@gmail.com>
2022-04-28 22:32:56 +0530

    Update

---
+++ CODE_CHEF/AA_23_TALLER.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+  class AA_23_TALLER
+  {
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+
+        int t = s.nextInt();
+
+        while(t-- > 0)
+        {
+
+            int x = s.nextInt();
+            int y = s.nextInt();
+
+            if((x  < y)
+            {
+                System.out.println("Y");
+            }
+            else
+                System.out.println("X");
+
+        }
+    }
+}
