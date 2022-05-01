9b0585c290f4256a75648e69ed2fc71123e9c490
Piyush096 <mishra.piyush1886@gmail.com>
2022-05-01 10:59:15 +0530

    Update

---
+++ test2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class test2{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+
+        int t = s.nextInt();
+
+        while(t-- > 0){
+
+            int x = s.nextInt();
+            int y = s.nextInt();
+            System.out.println((x*10) + (y*5));
+
+
+
+
+        }
+    }
+}
