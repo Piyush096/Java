417a94e81f39822dc3c4ec3ec6d491fbd3197c8f
Piyush096 <mishra.piyush1886@gmail.com>
2022-05-04 21:12:57 +0530

    Update

--- test2.java
+++ test2.java
@@ -3,17 +3,17 @@ class test2{
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
 
+        int x = s.nextInt();
+
+        while(x-- >0){
             int a = s.nextInt();
             int b = s.nextInt();
-            int c = s.nextInt();
-            int x = s.nextInt();
 
-            if(a == x || b == x || c == x){
-                System.out.println("yes");
+            if(a * 100 >= b * 10){
+                System.out.println("CLOTH");
+            }else{
+                System.out.println("DISPOSABLE");
             }
-            else{
-                System.out.println("no");
-            }
-
+        }
     }
 }
