24c8ae65fa688ade4dd4845bc84a86c77a5b86fa
Piyush096 <mishra.piyush1886@gmail.com>
2022-06-28 12:55:28 +0530

    Update

--- Test Cases/test.java
+++ Test Cases/test.java
@@ -5,24 +5,7 @@ public class test{
     public static void main(String [] args){
 
     Scanner input = new Scanner(System.in);
-    String [] symbols = new String[4];
-
-    int x = 0;
-
-    for(int i = 0; i< 4; i++){
-        symbols[i] = input.next();
-
-        if((symbols.equal(new String("X++"))) || (symbols.equal(new String("++X")))){
-           x = x + 1;
-        }
-        else if((symbols.equal(new String("X--"))) || (symbols.equal(new String("--X")))){
-            x = x - 1;
-        }
-
-    }
-        // String pot= Arrays.toString(symbols);
-        // System.out.print(pot);
-        System.out.println(x);
+    
 }
 
 }
