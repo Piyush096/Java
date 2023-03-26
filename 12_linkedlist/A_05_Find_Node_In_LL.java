c0da5f82e4b9b2091bb8d47639171c0b54732ac3
Piyush096 <mishra.piyush1886@gmail.com>
2023-03-26 20:00:28 +0530

    Update

---
+++ 12_linkedlist/A_05_Find_Node_In_LL.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+class Node{
+
+    int data;
+    Node next;
+
+    Node(int data){
+
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class A_05_Find_Node_In_LL {
+
+    public static Node take_Node(){
+
+        Node head = null;
+        Node tail = null;
+
+        Scanner in = new Scanner(System.in);
+
+         while(true)
+         {
+            int data = in.nextInt();
+
+
+            if (data == -1)
+                break;
+
+            Node currNode = new Node(data);
+
+            if(head == null){
+                head = currNode;
+                tail = currNode;
+            }
+            else{
+                tail.next = currNode;
+                tail = currNode;
+            }
+         }
+
+
+        return head;
+    }
+    public static Node printNode(Node head){
+
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data+ " ");
+            temp = temp.next;
+        }
+        System.out.println(" ");
+        return head;
+    }
+    public static int find_node(Node head, int n){
+
+        if(head == null){
+            return -1;
+        }
+
+        int i = 0;
+        while(head != null){
+            if(head.data == n){
+                return i;
+            }
+            head = head.next;
+            i++;
+        }
+        return -1;
+
+    }
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        Node head = take_Node();
+        printNode(head);
+        System.out.print("enter the node to find =  ");
+        int a = in.nextInt();
+        System.out.println("position of Node " + a + " is =  "+ find_node(head, a));
+    }
+}
---
+++ 12_linkedlist/Append_last_N_tofirst.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Append_last_N_tofirst {
+    public static Node takeNode(){
+
+        Node head = null;
+        Node tail = null;
+
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+
+            int data = in.nextInt();
+
+            if(data == -1)
+            break;
+
+            Node currNode = new Node(data);
+
+            if(head == null){
+                head = currNode;
+                tail = currNode;
+            }
+            else{
+                tail.next = currNode;
+                tail = currNode;
+            }
+
+        }
+        return head;
+    }
+    public static Node append_lastNto_first(Node head, int n){
+        if(n== 0 || head == null){
+            return head;
+        }
+        Node fast = head;
+        Node slow = head;
+        Node initial_head = head;
+
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
+        }
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        Node temp = slow.next;
+        slow.next = null;
+        fast.next = initial_head;
+        head = temp;
+
+        return temp;
+    }
+    public static Node printNode(Node head){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data+ " ");
+            temp = temp.next;
+        }
+        return head;
+    }
+    public static void main(String[] args){
+        Node head = takeNode();
+        head = append_lastNto_first(head, 3);
+        printNode(head);
+
+    }
+
+
+}
+
