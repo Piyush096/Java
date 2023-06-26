62fb50833a52a0870d01a05a75f8ea27c09a39bd
Piyush096 <mishra.piyush1886@gmail.com>
2023-06-26 20:13:16 +0530

    update

--- 12_linkedlist/A_13_reverse_ll.java
+++ 12_linkedlist/A_13_reverse_ll.java
@@ -42,20 +42,37 @@ public class A_13_reverse_ll {
         return head;
     }
 
-    public static Node reverse_ll(Node head){
-         if(head == null || head.next == null){
-            return head;
-         }
-         Node smallhead = reverse_ll(head.next);
-         Node tail = smallhead;
-         while(tail.next != null){
-            tail = tail.next;
-         }
-         tail.next = head;
-         head.next = null;
-         return smallhead;
-    }
+    // public static Node reverse_ll(Node head){
+    //      if(head == null || head.next == null){
+    //         return head;
+    //      }
+    //      Node smallhead = reverse_ll(head.next);
+    //      Node tail = smallhead;
+    //      while(tail.next != null){
+    //         tail = tail.next;
+    //      }
+    //      tail.next = head;
+    //      head.next = null;
+    //      return smallhead;
+    // }
     // another simplest form to reverse the linked list;
+
+    public static Node revNode(Node head){
+
+        Node previous = null;
+        Node current = head;
+        Node next = null;
+
+        while(current != null){
+
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+        return head;
+    }
 
     public static void print_ll(Node head){
 
@@ -71,7 +88,10 @@ public class A_13_reverse_ll {
 
     public static void main(String[] args){
         Node head = takNode();
-        head = reverse_ll(head);
+        print_ll(head);
+        // head = reverse_ll(head);
+        System.out.println("After revarsal of the linked list");
+        head = revNode(head);
         print_ll(head);
 
     }
